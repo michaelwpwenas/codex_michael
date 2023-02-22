6bf688b5d0f61b5e7e2524d1dedab300358a8095
Michael Wenas <michaelwenas@mail.ru>
2023-02-22 07:20:32 -0800

    update script

--- client/script.js
+++ client/script.js
@@ -32,7 +32,7 @@ function generateRandomID() {
   const time = Date.now();
   const randomNumber = Math.random();
   const hexString = randomNumber.toString(16);
-  return 'id-${time}-${hexString}';
+  return `id-${time}-${hexString}`;
 }
 
 function chatStripe (isAI, value, uniqueID) {
