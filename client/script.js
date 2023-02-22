4058b3f8a3ae3260160f63beba3741ae397b6696
Michael Wenas <michaelwenas@mail.ru>
2023-02-22 07:04:48 -0800

    update chat container

--- client/script.js
+++ client/script.js
@@ -2,7 +2,7 @@ import bot from './assets/bot.svg';
 import user from './assets/user.svg';
 
 const form = document.querySelector('form');
-const chatContainer = document.querySelector('#chatbot_container');
+const chatContainer = document.querySelector('#chat_container');
 
 let loadInterval;
 
